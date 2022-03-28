459b1f0721557b1fbf06ff31e22997ca9ad4d16f
Chin <chinmay.jb@gmail.com>
2022-03-28 18:57:19 +1100

    updated db

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ dbConnect()
 async function dbConnect(){
 
      try {
-         await mongoose.connect('mongodb+srv://sathya:sathyapr@cluster0.wrqpt.mongodb.net/sheyjobsudemy' , {useNewUrlParser : true});
+//          await mongoose.connect('mongodb+srv://sathya:sathyapr@cluster0.wrqpt.mongodb.net/sheyjobsudemy' , {useNewUrlParser : true});
+          await mongoose.connect('mongodb://localhost:27017/gmnportal', {useNewUrlParser : true, useUnifiedTopology: true});
          console.log('Mongo DB Connection success')
      } catch (error) {
          console.log('Mongo DB Connection failed')
