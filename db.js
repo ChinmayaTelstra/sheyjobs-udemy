b7689c2a74e8523f953d8bf83284c1a0e4400144
Chin <chinmay.jb@gmail.com>
2022-03-28 19:03:45 +1100

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,23 @@
 const mongoose = require("mongoose");
+const username = "chinmaya";
+const password = "Monza2022";
+const cluster = "cluster0.9tvmm";
+const dbname = "prototype";
+
 dbConnect()
 async function dbConnect(){
 
-     try {
-//          await mongoose.connect('mongodb+srv://sathya:sathyapr@cluster0.wrqpt.mongodb.net/sheyjobsudemy' , {useNewUrlParser : true});
-          await mongoose.connect('mongodb://localhost:27017/gmnportal', {useNewUrlParser : true, useUnifiedTopology: true});
-         console.log('Mongo DB Connection success')
-     } catch (error) {
+try 
+{
+//await mongoose.connect('mongodb+srv://sathya:sathyapr@cluster0.wrqpt.mongodb.net/sheyjobsudemy' , {useNewUrlParser : true});
+//await mongoose.connect('mongodb://localhost:27017/gmnportal', {useNewUrlParser : true, useUnifiedTopology: true});
+await mongoose.connect(`mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, {useNewUrlParser : true, useUnifiedTopology: true});
+console.log('Mongo DB Connection success')
+} 
+catch (error) 
+{
          console.log('Mongo DB Connection failed')
-     }
-
+}
 }
 
 module.exports = mongoose
